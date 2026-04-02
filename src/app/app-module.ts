dec1241d8960ebdb2f18699212b337ba15544b17
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2026-04-01 22:43:17 -0500

    primera sesion

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
+import { AppMaterialModule } from './app.material.module';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     App
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppMaterialModule,
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,45 @@
 import { Component, signal } from '@angular/core';
+import { AppMaterialModule } from './app.material.module';
+import { FormBuilder, FormsModule, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  standalone: false,
+  standalone:  false,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('proyecto');
+
+  formRegistra : any;
+   
+  constructor(private formBuilder: FormBuilder) {
+      console.log("App constructor called");
+  }
+
+  ngOnInit(): void {
+      console.log("App ngOnInit called");
+      
+      this.formRegistra = this.formBuilder.group({
+            nombre: [''],
+            correo: [''],
+       }) 
+
+  }
+
+  registra(){
+      //Swal.fire("Esto es angular!!!!");
+      Swal.fire({
+          title: "Do you want to save the changes?",
+          showDenyButton: true,
+          showCancelButton: true,
+          confirmButtonText: "Save",
+          denyButtonText: `Don't save`
+      }).then((result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) Swal.fire("Saved!", "", "success");
+          else if (result.isDenied) Swal.fire("Changes are not saved", "", "info");
+      });
+  }
+
 }
